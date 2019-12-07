b326eb317e1d3fbcf5654a092201587539b705f8
Ben Qiu <bqiu@dius.com.au>
2019-12-07 18:56:31 +1100

    Updates

---
+++ precache-manifest.cf8c519d2f654d034badb3c0a1f78069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2f1e65433d77a89f0951c2729e0a48",
+    "url": "/kids-math-generator/index.html"
+  },
+  {
+    "revision": "de1d9f5430b2f1e6812b",
+    "url": "/kids-math-generator/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "22dcf67d4c76db38da70",
+    "url": "/kids-math-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de1d9f5430b2f1e6812b",
+    "url": "/kids-math-generator/static/js/2.6f3d8654.chunk.js"
+  },
+  {
+    "revision": "e50005ea244c9c3a4db0afdcd501983f",
+    "url": "/kids-math-generator/static/js/2.6f3d8654.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22dcf67d4c76db38da70",
+    "url": "/kids-math-generator/static/js/main.1a0ae111.chunk.js"
+  },
+  {
+    "revision": "f968b9fd6f7ae551f2ea",
+    "url": "/kids-math-generator/static/js/runtime-main.6f1ce814.js"
+  }
+]);
