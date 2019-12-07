30840f44d19ab9e887371400a9aa68db06fc8cbb
Ben Qiu <bqiu@dius.com.au>
2019-12-07 18:53:08 +1100

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MathPage from './pages/MathPage'
 import Home from './pages/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Card, Row, Col, ListGroup } from 'react-bootstrap'
 
 const Home = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/grade/1/addition">Grade 1 Addition</Link>
-        </li>
-        <li>
-          <Link to="/grade/1/subtraction">Grade 1 Subtraction</Link>
-        </li>
-      </ul>
-    </div>
+    <Row className="mt-5">
+      <Col sm={4} className='m-auto'>
+        <Card>
+          <Card.Header>Grade 1 Math</Card.Header>
+          <Card.Body>
+            <Card.Text>Select the below generators</Card.Text>
+
+            <ListGroup as="ul" horizontal>
+              <ListGroup.Item as="li">
+                <Link to="/grade/1/addition">Addition</Link>
+              </ListGroup.Item>
+              <ListGroup.Item as="li">
+                <Link to="/grade/1/subtraction">Subtraction</Link>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>{' '}
+      </Col>
+    </Row>
   )
 }
 
